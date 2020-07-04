00df0b8003b5b429b55e4fd27e7a0135d6a34472
Fabio Motta <fabiomotta0311@gmail.com>
2020-07-04 02:17:08 -0300

    Isolate Button and Input Comp

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #f4ede8;
+
+  &::placeholder {
+    color: #666360;
+  }
+
+  & + input {
+    margin-top: 8px;
+  }
+`;
